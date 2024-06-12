64669c793087bcfd80b85ef1915aeb0722b531df
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-06-12 17:07:22 +0100

    commit

--- src/main/java/com/Diabetes/Controller/ConseilController.java
+++ src/main/java/com/Diabetes/Controller/ConseilController.java
@@ -1,15 +1,17 @@
-
+package com.Diabetes.Controller;
 
 import com.Diabetes.Service.ConseilsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/conseils")
 public class ConseilController {
-    @Autowired
-    private  ConseilsService conseilsService;
+    private final ConseilsService conseilsService;
+    private final UtilisateurRepository utilisateurRepository;
 
+    public ConseilController(ConseilsService conseilsService, UtilisateurRepository utilisateurRepository) {
+        this.conseilsService = conseilsService;
+        this.utilisateurRepository = utilisateurRepository;
+    }
 }
-
--- src/main/java/com/Diabetes/Models/LectureGlycemie.java
+++ src/main/java/com/Diabetes/Models/LectureGlycemie.java
@@ -10,6 +10,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 
 @Entity
 @Setter
