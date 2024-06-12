77dfd18e1449537806366e3d11d66fbe557e6185
oussama charafi <ou.charafi@gmail.com>
2024-06-12 11:29:29 +0100

    I'm working in the Package of sport

---
+++ src/main/java/com/Diabetes/Repository/SportRepository/ProgrammeRepository.java
@@ -0,0 +1,5 @@
+package com.Diabetes.Repository.SportRepository;
+import com.Diabetes.Models.Sport.Programme;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ProgrammeRepository extends JpaRepository<Programme, Long> {
+}
