30117d6247114c907c93a3840ec8cb39b1a1471d
oussama charafi <ou.charafi@gmail.com>
2024-06-13 10:04:37 +0100

    I onpend the confli

--- src/main/java/com/Diabetes/Controller/ConseilController.java
+++ src/main/java/com/Diabetes/Controller/ConseilController.java
@@ -1,15 +1,14 @@
 package com.Diabetes.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ConseilController {
-}
-
-
 
     @RequestMapping("/conseil")
     public String conseil(Model model) {
         return "conseilhtml";
     }
 }
-
